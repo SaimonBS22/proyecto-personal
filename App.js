1c60bb6cb7f08ac5ed2ee4ba0ae671f7422ad3fc
simon blaksley <simonblaksley@gmail.com>
2024-11-25 12:31:08 -0300

    guardando el proyecto

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable */
+/* prettier-ignore */
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ScrollView, Pressable} from "react-native";
+
+const imagen = require('./imagenes/header.jpg')
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return(
+  <View style={styles.container}>
+    <Image source={imagen} style={styles.imagenHeader}></Image>
+    <Pressable style={({pressed}) =>[
+      styles.texto,
+    {backgroundColor: pressed ? 'white' : 'none'}
+    ]}>
+      <Text>Carrito</Text>
+    </Pressable>
+    <StatusBar style='light' />
+  </View>
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    
   },
+  texto: {
+    position:'absolute',
+    top:25,
+    right:10,
+    color: 'white', 
+  },
+  imagenHeader:{
+    width:500,
+    height:75,
+    position:'absolute',
+    top:0
+  }
 });
